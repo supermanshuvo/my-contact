52ffbc1e03b7fb0c78c22154bc786d65d3155f06
supermanshuvo <shuvothesuperman@gmail.com>
2021-10-29 19:39:48 +0600

    log in set up done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import { BrowserRouter,Switch,Route } from 'react-router-dom';
-import Login from './components/Login';
-import NavBar from './components/NavBar'
-import Signup from './components/Signin';
-import Todo from './components/Todo';
+import React from 'react'
+import Signup from "./Signup"
+import { Container } from 'react-bootstrap'
+import { AuthProvider } from './contexts/AuthContext';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import DashBoard from './DashBoard'
+import Login from './Login';
+import PrivateRoute from './PrivateRoute';
+import ForgotPassword from './ForgotPassword';
+import UpdateProfile from "./UpdateProfile"
 
 function App() {
   return (
-    <>
-    <BrowserRouter>
-      <NavBar />
-      <Switch>
-        <Route exact path="/">
-          <Todo />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/signup">
-          <Signup />
-        </Route>
-      </Switch>
-    </BrowserRouter>
-    </>
+    <Container className="d-flex align-items-center justify-content-center" style={{ minHeight:"100vh"}}>
+      <div className="w-100" style={{maxWidth:"400px"}}>
+        <Router>
+        <AuthProvider>
+          <Switch>
+            <PrivateRoute exact path="/" component={DashBoard} />
+            <PrivateRoute path="/update-profile" component={UpdateProfile} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
+            <Route path="/forgot-password" component={ForgotPassword} />
+          </Switch>
+        </AuthProvider>
+        </Router>
+      </div>
+      </Container>
   );
 }
 
