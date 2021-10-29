dc6e8a5cc98c85af0587c6d94ae039aa6e4ebde7
supermanshuvo <shuvothesuperman@gmail.com>
2021-10-29 17:44:47 +0600

    firebase set up

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firebase-firestore'
+import Config from './config'
+
+firebase.initializeApp(Config)
+const auth = firebase.auth()
+const db = firebase.firestore()
+export {auth,db}
