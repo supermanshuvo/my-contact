d3435cad50141e276cd8d574ea6a7f3f602383ce
supermanshuvo <shuvothesuperman@gmail.com>
2021-10-30 08:37:21 +0600

    done

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { db } from "../firebase";
+import { useHistory } from "react-router-dom";
+export default function Todo({ user }) {
+  const [todoInput, setTodoInput] = useState("");
+  const [myTodos, setTodos] = useState([]);
+  const history = useHistory();
 
-export default function Todo() {
+  useEffect((user) => {
+    if (user) {
+      const result = db.collection("todos").doc(user.uid);
+      result.onSnapshot((docSnap) => {
+        if (docSnap.exists) {
+          console.log(docSnap.data().todos);
+          setTodos(docSnap.data().todos);
+        } else {
+          console.log(`No Docs`);
+        }
+      });
+    } else {
+      history.push("/");
+    }
+  }, []);
+
+  const addTodo = () => {
+    db.collection("todos")
+      .doc(user.uid)
+      .set({
+        todos: [...myTodos, todoInput],
+      });
+    setTodoInput("");
+  };
   return (
     <div className="center container">
       <h3>Todo</h3>
+      <div className="input-field">
+        <input
+          type="text"
+          name="addTodo"
+          id="addTodo"
+          placeholder="Enter your Todo"
+          value={todoInput}
+          onChange={(e) => setTodoInput(e.target.value)}
+        />
+        <button className="btn blue" onClick={() => addTodo()}>
+          Add
+        </button>
+      </div>
     </div>
   );
 }
