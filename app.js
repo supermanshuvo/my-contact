9003e5bb2d36f998ebdb6e5f27e8a021b7c23a19
supermanshuvo <shuvothesuperman@gmail.com>
2021-11-13 01:06:47 +0600

    geting old error

--- app.js
+++ app.js
@@ -18,14 +18,61 @@ let registerUserButton= document.getElementById('registerUserButton');
 let verifyUser = document.getElementById('verifyUser');
 let register =document.getElementById('register');
 let loginForm= document.getElementById('loginForm');
-function saveDate(){
-    console.log('register save');
-    console.log(fname.value,lname.value,registerEmail.value,registerPassword.value,phone.value,bodate.value,address.value);
+
+function addData(uid,person){
+    console.log('I am in addData')
+    console.log(uid,person)
+    // firebase.firestore().collection("user-information").doc(uid).set({
+    //     firstName: person.firstName,
+    //     lastName: person.lastName,
+    //     email: person.email,
+    //     password: person.password,
+    //     number: person.number,
+    //     birthDay: person.birthDay,
+    //     address: person.address
+    // })
+    // .then(() => {
+    //     console.log("Document successfully written!");
+    // })
+    // .catch((error) => {
+    //     console.error("Error writing document: ", error);
+    // });
+}
+
+function createUser(person){
+    console.log('Request Initiated')
+    firebase.auth().createUserWithEmailAndPassword(person.email, person.password)
+        .then((userCredential)=>{
+            let user = userCredential.user;
+            let uid = user.uid;
+            addData(uid,person)
+        })
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorCode);
+            console.log(errorMessage);
+          });
+}
+
+function submitRegister(){
+    const person={
+        firstName: fname.value,
+        lastName: lname.value,
+        email: registerEmail.value,
+        password: registerPassword.value,
+        number: phone.value,
+        birthDay: bodate.value,
+        address: address.value
+    }
+    createUser(person);
+    // console.log('register save');
+    // console.log(fname.value,lname.value,registerEmail.value,registerPassword.value,phone.value,bodate.value,address.value);
     register.reset();
     // console.log(fname.value)
 }
 function login(){
-    console.log(emailLogin.value,passwordLogin.value);
+    // console.log(emailLogin.value,passwordLogin.value);
     loginForm.reset();
     // console.log('login btn')
     // console.log(emailLogin,passwordLogin)
@@ -48,7 +95,7 @@ function logInFormFunc(){
     loginButton.className='btn btn-primary btn-sm'
     registerButton.className = 'btn btn-primary btn-sm d-none'
 }
-registerButton.addEventListener('click',saveDate);
+registerButton.addEventListener('click',submitRegister);
 loginButton.addEventListener('click',login);
 registerUserButton.addEventListener('click',registerForm);
 verifyUser.addEventListener('click',logInFormFunc);
---
+++ init-firebase.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDGRwwgziQDqHCEauFMKNltGbu--Qj9Odw",
+    authDomain: "learn-firebae.firebaseapp.com",
+    projectId: "learn-firebae",
+    storageBucket: "learn-firebae.appspot.com",
+    messagingSenderId: "508063845650",
+    appId: "1:508063845650:web:25fc1d2a6cf5949e3077cb",
+    measurementId: "G-3TR292PEQM"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  
